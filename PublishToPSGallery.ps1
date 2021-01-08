9e36c9b5a17d292e2290a9ab64acc153d1a502aa
Dr John Tunnicliffe <john@bovi.co.uk>
2021-01-08 00:37:04 +0000

    Published to PowerShell Gallery

--- PublishToPSGallery.ps1
+++ PublishToPSGallery.ps1
@@ -1,26 +1,34 @@
+
 param (
     [Parameter(Mandatory)]
     [string] $ApiKey
 )
 
-
-$VerbosePreference = 'Continue';
+#$VerbosePreference = 'Continue';
 $ErrorActionPreference = 'Stop';
+
 $baseDir = $PSScriptRoot;
 
-.\UpdateHelp.ps1
+$PsdPath = Resolve-Path "$PSScriptRoot\DeployCube\DeployCube.psd1";
+$psd = Import-PowershellDataFile $PsdPath;
+$ModuleVersion = $psd.ModuleVersion;
+Write-Host "Module Version to be published to the Gallery: $ModuleVersion" -ForegroundColor Yellow;
 
-try {
-    $buildDir = "$baseDir\DeployCube";
-    Write-Information $buildDir;
-    Write-Verbose 'Importing PowerShellGet module'
-    $psGet = Import-Module PowerShellGet -PassThru -Verbose:$false
-    & $psGet { [CmdletBinding()] param () Install-NuGetClientBinaries -CallerPSCmdlet $PSCmdlet -BootstrapNuGetExe -Force }
+$confirmation = Read-Host "Are you Sure?  Type Y to Proceed."
+if ($confirmation -eq 'Y') {        
+    .\UpdateHelp.ps1
+    try {
+        $buildDir = "$baseDir\DeployCube";
+        Write-Information $buildDir;
+        Write-Verbose 'Importing PowerShellGet module'
+        $psGet = Import-Module PowerShellGet -PassThru -Verbose:$false
+        & $psGet { [CmdletBinding()] param () Install-NuGetClientBinaries -CallerPSCmdlet $PSCmdlet -BootstrapNuGetExe -Force }
 
-    Write-Host 'Publishing module using PowerShellGet'
-    $null = Publish-Module -Path $buildDir -NuGetApiKey $ApiKey -Confirm:$true;
-}
-catch {
-    Write-Error -ErrorRecord $_
-    exit 1
+        Write-Host 'Publishing module using PowerShellGet'
+        $null = Publish-Module -Path $buildDir -NuGetApiKey $ApiKey -Confirm:$false;
+    }
+    catch {
+        Write-Error -ErrorRecord $_
+        exit 1
+    }
 }
